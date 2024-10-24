af4b8875759f956d01657f18cc8df69b6260fa92
PabloH <pablohenriqq01@gmail.com>
2024-10-23 22:30:19 -0300

    Fazendo catalogo.html e ligando ao Python e ja listando por ordem de id

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+#pip install Flask-SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -10,18 +11,15 @@ class Bebidas:
         self.preco = preco_bebidas
 
 
-
 app = Flask(__name__)
 
 #config banco de dados PostgreSQL - Pablo
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4701@localhost/db_adega'
 
 #config banco de dados MySQL - pip install pymysql
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://usuario:senha@localhost/db_adega'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://usuario:senha@localhost/db_adega'
 
 db =SQLAlchemy(app)
-
-
 
 class Bebidas(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True )
@@ -30,3 +28,15 @@ class Bebidas(db.Model):
     marca_produto = db.Column(db.String(150), nullable = False)
     preco_produto = db.Column(db.Float, nullable = False)
     
+@app.route("/")
+def main():
+    return "Rota principal do projeto"
+
+@app.route("/catalogo")
+def catalogo():
+    lista_bebidas = Bebidas.query.order_by(Bebidas.id)
+
+    return render_template("catalogo.html",
+                           lista_bebidas = lista_bebidas)
+
+app.run()
