f378f9afd8597e11395255e5e28adf42a1ccf8d0
PabloH <pablohenriqq01@gmail.com>
2024-11-01 21:03:04 -0300

    Nova funcionalidade na pagida de cadastro! agora o cadastro vai direto para o banco de dados.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class Bebidas:
 app = Flask(__name__)
 
 #config banco de dados PostgreSQL - Pablo
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4701@localhost/db_adega'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4701@localhost/db_adega'
 
 #config - Joao
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:CASTELO2004@localhost/testedb'
@@ -23,7 +23,7 @@ app = Flask(__name__)
 #config banco de dados MySQL - pip install pymysql
 
 # config - joao
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:castelo12@localhost/db_adega'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:castelo12@localhost/db_adega'
 # Config - Dennis
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:4668@localhost/db_adega'
 
@@ -61,6 +61,16 @@ def catalogo_editar():
     return render_template("catalogo_edit.html",
                            catalogo_bebidas = lista_bebidas)
 
+@app.route("/inserir", methods=['POST',])
+def adicionar_bebidas():
+    tipo = request.form['tipo_bebida']
+    nome = request.form['nome_bebida']
+    marca = request.form['marca']
+    valor = float(request.form['valor'])
 
+    nova_bebida = Bebidas(tipo_produto = tipo, nome_produto = nome, marca_produto = marca, preco_produto = valor)
+    db.session.add(nova_bebida)
+    db.session.commit()
+    return redirect('/catalogo')
 
 app.run()
